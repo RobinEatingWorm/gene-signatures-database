9e274c2e0ea342b2aa0d82274df8d9a958c0372e
RobinEatingWorm <gavinpu27@gmail.com>
2024-03-04 00:15:26 -0500

    Fix run functions

--- utils/run.py
+++ utils/run.py
@@ -1,57 +1,60 @@
 from openai import OpenAI
-
-import pandas as pd
+from openai.types.chat import ChatCompletion
 
 import json
 import re
-from typing import Any
+
+with open("settings/params.json") as file:
+    params = json.load(file)
+    prompts_dir = params["prompts"]
 
 
-def generate_gene_regex(filename: str, column: str) -> str:
+def get_prompt(prompt_number: int) -> str:
     """
-    Generate a regular expression containing genes from a TSV.
-    :param filename: The filename of the TSV.
-    :param column: The column label of a column containing genes.
-    :return: A regular expression that matches genes present in the TSV column.
+    Read a prompt from a file and format it correctly.
+    :param prompt_number: The number in the prompt filename.
+    :return: A string containing the formatted rompt.
     """
 
-    # Load the TSV and get the column
-    gene_list = pd.read_csv(filename, sep="\t", dtype=object)[column]
+    # Read all lines from the file
+    with open(f"{prompts_dir}/prompt_{prompt_number:02d}.txt") as file:
+        prompt = file.readlines()
 
-    # Remove empty values and duplicates
-    gene_list = gene_list.dropna().unique()
-
-    # Join genes together to form a regular expression
-    return r"|".join(gene_list)
+    # Replace newlines with spaces
+    return " ".join([line.strip() for line in prompt])
 
 
-def get_relevant_lines(article: list[str], gene_regex: str) -> list[str]:
+def get_relevant_lines(article_filename: str, gene_regex: str) -> list[str]:
     """
     Get all lines in an article (excluding references) containing any gene
     symbols specified in a regular expression.
-    :param article: A list of all lines in the article.
+    :param article_filename: The filename of the article.
     :param gene_regex: A regular expression that matches gene symbols.
     :return: A list of lines with gene symbols matching the regular expression.
     """
 
+    # Read all lines in the article
+    with open(article_filename, errors="ignore") as file:
+        article = file.readlines()
+
     # Initialize a list to store relevant lines
     article_relevant = []
 
-    # Find lines with gene symbols excluding references
+    # Find lines with at least two different gene symbols excluding references
     for line in article:
-        if line == "=== Refs\n":
+        if line == "==== Refs\n":
             break
-        if re.search(gene_regex, line):
+        if len(set(re.findall(gene_regex, line))) >= 2:
             article_relevant.append(line)
     return article_relevant
 
 
-def create_chat_completion(prompt: str, text: str) -> Any:
+def create_chat_completion(prompt: str, text: str) -> ChatCompletion:
     """
     Get a response for the given chat conversation.
     :param prompt: A prompt to set the behaviour of the LLM.
     :param text: A message that the LLM should respond to.
-    :return: 
+    :return: A Chat Completions API response.
     """
 
     # API key
@@ -61,13 +64,12 @@ def create_chat_completion(prompt: str, text: str) -> Any:
     # Run the LLM
     client = OpenAI(api_key=api_key)
     response = client.chat.completions.create(
-        model="gpt-3.5.turbo",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": prompt},
             {"role": "user", "content": text},
         ],
-        response_format={"type": "json-object"},
+        response_format={"type": "json_object"},
         temperature=0,
     )
-    print(type(response)) # FIX
     return response
